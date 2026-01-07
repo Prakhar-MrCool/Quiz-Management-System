a8f079efc2985baa09e3308c2bd1c5d920371d8e
Prakhar-MrCool <prakhartewari21@gmail.com>
2026-01-07 20:38:37 +0530

    Initial Commit

---
+++ src/test/java/com/quiz/Quiz_Management_System/QuizManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quiz.Quiz_Management_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
